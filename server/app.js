3af5002a305975673676e259a2398842a9618e56
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-07-10 19:26:00 -0400

    added dotenv

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
+const dotenv = require("dotenv");
+dotenv.config();
 
-// viewed at http://localhost:8080
+const PORT = process.env.PORT || 8080;
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
